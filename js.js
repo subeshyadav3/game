27957b1ab86d338390cec577d8b0d8e9e092de59
subeshyadav3 <41501661+subeshyadav3@users.noreply.github.com>
2024-01-10 13:57:00 +0545

    Update js.js

--- js.js
+++ js.js
@@ -1,77 +1,58 @@
-let btn=document.querySelector("button");
-let body=document.querySelector("body");
-let mode="light";
-btn.onclick=()=>{
-	if(mode==="light"){
-		mode="dark";
-		body.style.backgroundColor="white";
-		body.style.color="blue";
-		console.log("light");
-		// btn.backgroundColor="red";
-		
+let btn = document.querySelector("button");
+let body = document.querySelector("body");
+let mode = "light";
 
-	}
-	else{
-		mode="light";
-		body.style.backgroundColor="skyblue";
-		body.style.color="white";
-		console.log("black");
-	}
+btn.onclick = () => {
+    if (mode === "light") {
+        mode = "dark";
+        body.style.backgroundColor = "white";
+        body.style.color = "blue";
+        console.log("light");
+    } else {
+        mode = "light";
+        body.style.backgroundColor = "skyblue";
+        body.style.color = "white";
+        console.log("black");
+    }
+};
+
+// Game Logic
+
+function autoSelect() {
+    const options = ['rock', 'paper', 'scissor'];
+    const choose = Math.floor(Math.random() * 3);
+    return options[choose];
 }
 
-// real coding of game 
+let comScore = 0;
+let humanScore = 0;
 
+let choices = document.querySelectorAll("#choice");
+let msg = document.querySelector("#result");
+let comp = document.querySelector("#comp");
+let hum = document.querySelector("#hum");
 
-//auto generate one 
-
-function autoSelect(){
-	options=['rock','paper','scissor'];
-	const choose=Math.floor(Math.random()*3);
-	return options[choose];
-}
-
-
-let comScore=0;
-let HumanScore=0;
-
-
-let choices=document.querySelectorAll("#choice");
-choices.forEach((choice)=>{
-	console.log(choice);
-	choice.addEventListener("click",()=>{
-		const choiceName=choice.getAttribute("name");
-
-		playGame(choiceName);
-	});
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const choiceName = choice.getAttribute("name");
+        playGame(choiceName);
+    });
 });
 
+function playGame(choiceName) {
+    const valueChoosen = autoSelect();
+    console.log("comp", valueChoosen);
+    console.log("user", choiceName);
 
-let msg=document.querySelector("#result");
-let comp=document.querySelector("#comp");
-let hum=document.querySelector("#hum");
-
-function playGame(choiceName){
-	const valueChoosen=autoSelect();
-	console.log("comp",valueChoosen);
-	console.log("user",choiceName);
-		if(valueChoosen=== choiceName){
-			msg.innerText=" Game Draw";
-		}
-		else{
-			if((valueChoosen==="rock" && choiceName==="scissor") || (valueChoosen==="paper" && choiceName==="rock") || (valueChoosen==="scissor" && choiceName==="paper")){
-				comScore=comScore+1;
-				console.log("comp score",comScore);
-				msg.innerText="Computer Won Here!";
-				comp.innerText=comScore;
-			}
-			else{
-				HumanScore=HumanScore+1;
-				msg.innerText="You won!!!";
-				hum.innerText=HumanScore;
-			}
-
-
-		}
-
-}
-
+    if (valueChoosen === choiceName) {
+        msg.innerText = "Game Draw";
+    } else {
+        if (
+            (valueChoosen === "rock" && choiceName === "scissor") ||
+            (valueChoosen === "paper" && choiceName === "rock") ||
+            (valueChoosen === "scissor" && choiceName === "paper")
+        ) {
+            comScore++;
+            console.log("comp score", comScore);
+            msg.innerText = "Computer Won Here!";
+            comp.innerText = comScore;
